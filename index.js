634cba5219fb6bf8f637c2b28184b07d7f4f8248
Layomi <layomilawore@gmail.com>
2026-09-17 08:38:23 +0100

    implementing display suggestions functionality logic

--- index.js
+++ index.js
@@ -19,20 +19,56 @@ async function fetchCities() {
 }
 fetchCities();
 
+function commaSeparateNumber(val) {
+  val = val.replace(",", "");
+  var array = val.split("");
+  var index = -3;
+  while (array.length + index > 0) {
+    array.splice(index, 0, ",");
+    // Decrement by 4 since we just added another unit to the array.
+    index -= 4;
+  }
+  return array.join("");
+}
+
+function numericWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 function findMatchedCities(wordToMatch, cities) {
   return cities.filter((place) => {
     // place must match word entered in the search
     const regex = new RegExp(wordToMatch, "gi");
     return place.city.match(regex) || place.state.match(regex);
-    console.log(place);
   });
 }
 
 function displayCities(e) {
   const matchArray = findMatchedCities(this.value, cities);
-  console.log(matchArray);
+
+  const html = matchArray
+    .map((place) => {
+      // style the city name
+
+      const regex = new RegExp(this.value, "gi");
+      const cityName = place.city.replace(
+        regex,
+        ` <span class='hl'>${this.value}</span>`,
+      );
+      const stateName = place.state.replace(
+        regex,
+        ` <span class='hl'>${this.value}</span>`,
+      );
+      return `<li>
+              <span class='name'>${cityName}, ${stateName}</span>
+              <span class='population'>${numericWithCommas(place.population)}</span>
+            </li>`;
+    })
+    .join("");
+  suggestions.innerHTML = html;
 }
 
 search.addEventListener("change", displayCities);
 search.addEventListener("keyup", displayCities);
 suggestions.addEventListener("enter", findMatchedCities);
+x;
